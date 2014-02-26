3466be9f58642206d830521c1dd251d6e27dfe2a
jasonhodges <jason@jasonhodges.me>
2014-02-26 15:13:16 +0000

    initial commit

---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+app.controller('PostsCtrl', function ($scope, $location, Post) {
+    if ($location.path() === '/') {
+    $scope.posts = Post.all;
+    }
+    
+    $scope.post = {url: 'http://', title: ''};
+    
+    $scope.submitPost = function () {
+        Post.create($scope.post).then(function (ref) {
+            $location.path('/posts/' + ref.name());
+        });
+    };
+    
+    $scope.deletePost = function (postId) {
+        Post.delete(postId);
+    };
+    $scope.upVotePost = function (postId, upVoted) {
+  if (upVoted) {
+    Post.clearVote(postId, upVoted);
+  } else {
+    Post.upVote(postId);
+  }
+};
+ 
+$scope.downVotePost = function (postId, downVoted) {
+  if (downVoted) {
+    Post.clearVote(postId, !downVoted);
+  } else {
+    Post.downVote(postId);
+  }
+};
+ 
+$scope.upVoted = function (post) {
+  return Post.upVoted(post);
+};
+ 
+$scope.downVoted = function (post) {
+  return Post.downVoted(post);
+};
+});
